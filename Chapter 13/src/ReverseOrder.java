847c9aad7f6e639389f64a81720c7f8663ee8289
walt0194 <waltb18082@isd273.org>
2017-01-27 13:48:03 -0600

    Add Chapter 13 project + Reverse Order program

---
+++ Chapter 13/src/ReverseOrder.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ReverseOrder {
+
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("How many numbers will you enter?");
+		
+		int arrayLength = s.nextInt();
+		
+		int [] numbers = new int[arrayLength];
+		
+		System.out.println("Enter numbers to fill array");
+		
+		for(int i=0; i<numbers.length; i++)
+		{
+			numbers[i] = s.nextInt();
+		}
+		
+		for(int i=arrayLength-1; i>=0; i--)
+		{
+			System.out.println(numbers[i]);
+		}
+	}
+
+}
